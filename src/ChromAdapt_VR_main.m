42a9a36b261d3a450a70191e90fddf0de35e36ca
aa6588 <aa6588@rit.edu>
2024-09-29 13:17:03 -0400

    Update ChromAdapt_VR_main.m

--- src/ChromAdapt_VR_main.m
+++ src/ChromAdapt_VR_main.m
@@ -6,10 +6,10 @@ function [participant] = ChromAdapt_VR_main
 % Adapted from 'Ambient_main.m' by Jaclyn Pytlarz
 %
 % set --action ('train','session')
-temptag = 'session';
+%temptag = 'session';
 %
 % define path to participant folder
-paths.participant  = '\\marvin.dsv.eng.dolby.net\avsg\Users\aaven\HK_Exp\HK_2AFC_Only\Participants\';
+paths.participant  = 'C:\Users\Andrea\Documents\GitHub\ColorCharacterization\src\Participants\';
 addpath(genpath('C:\Users\orange\Documents\GitHub\ColorCharacterization\utils\'))
 addpath(genpath('C:\Users\orange\Documents\GitHub\MCSL-Tools\Convert\'))
 % cd into the folde  r
@@ -34,10 +34,10 @@ participant.ID = pID;
 clear tmp name lastParticipant;
 %
 % Gather user data
-if strcmpi(temptag,'train') ~= 1
-    [f,participant]=uiGui_guide(participant,paths); waitfor(f);
-    save([paths.participant sprintf('Participant_%03g.mat',participant.ID)],'participant');
-end
+% if strcmpi(temptag,'train') ~= 1
+%     [f,participant]=uiGui_guide(participant,paths); waitfor(f);
+%     save([paths.participant sprintf('Participant_%03g.mat',participant.ID)],'participant');
+% end
 %
 %% Run the Experiment Session
 %
