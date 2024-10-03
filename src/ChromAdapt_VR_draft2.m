0c592fc9ae492f63deb665c34e3d1769a7040713
aa6588 <aa6588@rit.edu>
2024-10-03 15:02:53 -0400

    works! Random lightness levels under 1 illum.
    records selected rgb and lab in participant struct. can convert to table using rownames and fieldnames later

--- src/ChromAdapt_VR_draft2.m
+++ src/ChromAdapt_VR_draft2.m
@@ -1,7 +1,7 @@
-function [participant] = ChromAdapt_VR_draft2(participant,paths)
+%function [participant] = ChromAdapt_VR_draft2(participant,paths)
 %% startup
-addpath(genpath('C:\Users\orange\Documents\GitHub\ColorCharacterization\utils\'))
-addpath(genpath('C:\Users\orange\Documents\GitHub\MCSL-Tools\Convert\'))
+%addpath(genpath('C:\Users\orange\Documents\GitHub\ColorCharacterization\utils\'))
+%addpath(genpath('C:\Users\orange\Documents\GitHub\MCSL-Tools\Convert\'))
 
 %% Main function for Color Constancy Experiment VR
 % Create the connection
@@ -24,80 +24,83 @@ illum_rand = randperm(length(illum_color));
 
 %patch lightness
 L_star = {'L40' 'L55' 'L70'};
-
+rownames = {'RGB Select','Lab Select'};
 %% Start Experiment
 
 %start with white illum always
 rand_L = L_star(randperm(length(L_star)));
+participant = struct();
 for L = 1:length(L_star)
-
- participant.w.rand_L(L) = run_experiment(RGB_white_illum,rand_L(L));
-
+    [participant.w(1).(rand_L{L}), participant.w(2).(rand_L{L})] = run_experiment('w',RGB_white_illum,rand_L{L},RGB_grid,LAB_grid,t);
 end
 
 
 % save participant data
-save([paths.participant sprintf('Participant_%03g.mat',participant.ID)],'participant');  
-%% experiment function
+%save([paths.participant sprintf('Participant_%03g.mat',participant.ID)],'participant');  
+% experiment function
 
-% Run through RGB values by pressing key to advance
+%% Run through RGB values by pressing key to advance
 
-function [selection] = run_experiment(illum_color,lightness_level,rgb_grids,lab_grids)
+function [selection,lab_selection] = run_experiment(illum_color,illumRGB,lightness_level,rgb_grids,lab_grids,connect)
 
+%set vars
 grid = rgb_grids.(illum_color).(lightness_level);
 lab = lab_grids.(illum_color).(lightness_level);
 selection = zeros(1,3);
+lab_selection = zeros(1,3);
 
+%set lights
+a = "start";
+fwrite(connect, "Light:" + illumRGB(1) + "," + illumRGB(2) + "," + illumRGB(3));
+pause(1/8)
 %random starting point
 col_idx = randi(size(grid, 1));
 row_idx = randi(size(grid, 1));
-a = 'start';
-disp("Starting Lab:" + Lab_vals(row_idx, col_idx, 1) + "," + Lab_vals(row_idx,col_idx, 2) + "," ...
-        + Lab_vals(row_idx,col_idx, 3))   
 
-while ~strcmp(a, "next_trial")
-    
-    % Send illum and patch info to Unreal
-    fwrite(t, "Light:" + illum_color(1) + "," + illum_color(2) + "," + illum_color(3));
-    pause(1)
-    fwrite(t, "Value:" + grid(row_idx, col_idx, 1) + "," + grid(row_idx,col_idx, 2) + "," ...
-        + grid(row_idx,col_idx, 3));
+disp("Starting Lab:" + lab(row_idx, col_idx, 1) + "," + lab(row_idx,col_idx, 2) + "," ...
+        + lab(row_idx,col_idx, 3)) 
 
-    %search for key press from Unreal (a) answer
-    a = fscanf(t, '%s\n');
+    while ~strcmp(a, "next_trial")
 
+        % Send patch info to Unreal
+        fwrite(connect, "Value:" + grid(row_idx, col_idx, 1) + "," + grid(row_idx,col_idx, 2) + "," ...
+            + grid(row_idx,col_idx, 3));
+        %search for key press from Unreal (a) answer
+         a = fscanf(connect, '%s\n');
+        disp("RGB Value:" + grid(row_idx, col_idx, 1) + "," + grid(row_idx,col_idx, 2) + "," ...
+            + grid(row_idx,col_idx, 3));
+        disp("Lab:" + lab(row_idx, col_idx, 1) + "," + lab(row_idx,col_idx, 2) + "," ...
+            + lab(row_idx,col_idx, 3))
 
-switch a
-    case "rightarrow"
-        disp('right')
-        if col_idx < size(grid, 2)
-           col_idx = col_idx + 1;
-        end
-    case "leftarrow"
-        disp('left')
-        if col_idx > 1
-           col_idx = col_idx - 1;
-        end
-    case "uparrow"
-        disp('up')
-        if row_idx > 1
-           row_idx = row_idx - 1;
-        end
-    case "downarrow"
-        disp('down')
-        if row_idx < size(grid, 1)
-           row_idx = row_idx + 1;
-        end
-    case "enter"
-        disp('enter')
-        selection(1,:) = grid(row_idx,col_idx,:);
-        a = 'next trial';
-end  
+    switch a
+        case "rightarrow"
+            disp('right')
+            if col_idx < size(grid, 2)
+                col_idx = col_idx + 1;
+            end
+        case "leftarrow"
+            disp('left')
+            if col_idx > 1
+               col_idx = col_idx - 1;
+            end
+        case "uparrow"
+            disp('up')
+            if row_idx > 1
+               row_idx = row_idx - 1;
+            end
+        case "downarrow"
+            disp('down')
+            if row_idx < size(grid, 1)
+               row_idx = row_idx + 1;
+            end
+        case "enter"
+            disp('enter')
+            selection(1,:) = grid(row_idx,col_idx,:);
+            lab_selection(1,:) = lab(row_idx,col_idx,:);
+            a = 'next_trial';
+            disp(a)
+    end  
 
-    disp("RGB Value:" + grid(row_idx, col_idx, 1) + "," + grid(row_idx,col_idx, 2) + "," ...
-        + grid(row_idx,col_idx, 3));
-    disp("Lab:" + lab(row_idx, col_idx, 1) + "," + lab(row_idx,col_idx, 2) + "," ...
-        + lab(row_idx,col_idx, 3))
-    %press key in Unreal 
-
+    end
 end
+%end
