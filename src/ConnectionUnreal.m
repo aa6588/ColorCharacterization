6286d0ce264c0fc21e2f1d3b2efd5340fa31be2a
aa6588 <aa6588@rit.edu>
2024-12-11 18:07:09 -0500

    code for measuring labs

--- src/ConnectionUnreal.m
+++ src/ConnectionUnreal.m
@@ -304,59 +304,77 @@ end
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% load Lab_RGBs.mat %1-125 is cube rgb values. 126-x is lab values
-% %cont=0;
-% clear range
-% RGB_forLabs = RGB_forLabs_Blue;
-% RGB_labs = round(RGB_forLabs.*255); %1-125 is RGB cube, 126-end is labs
-% range = double(RGB_forLabs);
-% 
-% for i = 1
-% %for i = 1:size(RGB_labs, 1)
-%     tic
-% 
-%     fwrite(t, "Value:" + range(i, 1) + "," + range(i, 2) + "," ...
-%         + range(i, 3));
-%     a = fscanf(t, '%s\n');
-% 
-%     while ~strcmp(a, "SHOT")
-%         a = fscanf(t, '%s\n');
-%         fwrite(t, "Value:" + range(i, 1) + "," + range(i, 2) + ...
-%             "," + range(i, 3));
-%     end
-% 
-%     disp("Value:" + range(i, 1) + "," + range(i, 2) + "," + range(i, 3))
-%     pause(3)
-%     Validation_lab(i) = cs2000.measure;
-%     xyzObtain_lab(i,:)=Validation_lab(i).color.XYZ';
-% 
-%     if i > 1
-%         while max(abs(xyzObtain_lab(i,:) - xyzObtain_lab(i-1,:))) < .08 
-%             fwrite(t, "Value:" + range(i, 1) + "," + range(i, 2) + "," ...
-%             + range(i, 3));
-%             a = fscanf(t, '%s\n');
-% 
-%         while ~strcmp(a, "SHOT")
-%             a = fscanf(t, '%s\n');
-%             fwrite(t, "Value:" + range(i, 1) + "," + range(i, 2) + ...
-%             "," + range(i, 3));
-%         end
-%     disp('Measurement is the same as previous, taking a new measurement...')
-%     disp("Retake Value:" + range(i, 1) + "," + range(i, 2) + "," + range(i, 3))
-%     pause(3)
-%     Validation_lab(i) = cs2000.measure;
-%     xyzObtain_lab(i,:)=Validation_lab(i).color.XYZ';
-%         end
-% 
-%     end
-% 
-%     t_time = toc;
-%     disp(['It took ', num2str(t_time), ' s']);
-%     disp(['Trial #: ', num2str(i),' out of ',num2str(size(RGB_labs, 1))])
-%     disp '-------------------------------------------'
-% 
-% end       
-% 
+load RGBs_grids_struct.mat %1-125 is cube rgb values. 126-x is lab values
+load LAB_grid_struct.mat
+
+%one illum at a time
+level = {'L40','L55','L70'};
+illum = 'w';
+
+%loop lightness
+for j = 1:3
+ RGB_forLabs = RGB_grid.(illum).(level{j});
+ RGBs = [];
+%rand row and col
+ rand_row = randperm(size(RGB_forLabs,1),min(size(RGB_forLabs,1),10));
+ rand_col = randperm(size(RGB_forLabs,2),min(size(RGB_forLabs,2),10));
+ rand_idx.(illum).(level{j}).rand_row = rand_row;
+ rand_idx.(illum).(level{j}).rand_col = rand_col;
+ min_idx = min(length(rand_row),length(rand_col));
+ for sample = 1:min_idx
+     RGBs(sample,:) = RGB_forLabs(rand_row(sample),rand_col(sample),:);
+     rand_idx.(illum).(level{j}).Labs(sample,:) = LAB_grid.(illum).(level{j})(rand_row(sample),rand_col(sample),:);
+ end
+rand_idx.(illum).(level{j}).RGBs = RGBs;
+
+%measure
+for i = 1:size(RGBs, 1)
+    tic
+
+    fwrite(t, "Value:" + RGBs(i,1) + "," + RGBs(i,2) + "," ...
+        + RGBs(i,3));
+    a = fscanf(t, '%s\n');
+
+    while ~strcmp(a, "SHOT")
+        a = fscanf(t, '%s\n');
+        fwrite(t, "Value:" + RGBs(i, 1) + "," + RGBs(i, 2) + ...
+            "," + RGBs(i, 3));
+    end
+
+    disp("Value:" + RGBs(i,1) + "," + RGBs(i,2) + "," + RGBs(i,3))
+    pause(1)
+    Validation_lab = cs2000.measure;
+    rand_idx.(illum).(level{j}).XYZs(i,:) =Validation_lab.color.XYZ';
+   % rand_idx.(illum).(level{j}).Labs(i,:) = LAB_grid.(illum).(level{j})(rand_row(i),rand_col(i),:);
+
+    % if i > 1
+    %     while max(abs(xyzObtain_lab(i,:) - xyzObtain_lab(i-1,:))) < .08 
+    %         fwrite(t, "Value:" + range(i, 1) + "," + range(i, 2) + "," ...
+    %         + range(i, 3));
+    %         a = fscanf(t, '%s\n');
+    % 
+    %     while ~strcmp(a, "SHOT")
+    %         a = fscanf(t, '%s\n');
+    %         fwrite(t, "Value:" + range(i, 1) + "," + range(i, 2) + ...
+    %         "," + range(i, 3));
+    %     end
+    % disp('Measurement is the same as previous, taking a new measurement...')
+    % disp("Retake Value:" + range(i, 1) + "," + range(i, 2) + "," + range(i, 3))
+    % pause(3)
+    % Validation_lab(i) = cs2000.measure;
+    % xyzObtain_lab(i,:)=Validation_lab(i).color.XYZ';
+    %     end
+    % 
+    % end
+
+    t_time = toc;
+    disp(['It took ', num2str(t_time), ' s']);
+    disp(['Trial #: ', num2str(i),' out of ',num2str(size(rand_row, 1))])
+    disp '-------------------------------------------'
+
+end       
+end
+%test(1,:) = LAB_grid.r.L40(rand_row(3),rand_col(3),:)
 % save(save_filename, 'Red', 'Blue', 'Green', 'Gray', 'White','Validation_rand', 'RGB_forLabs','Validation_lab','PredefinedRGB');
 
 % %fwrite(t,"DONE:0");
