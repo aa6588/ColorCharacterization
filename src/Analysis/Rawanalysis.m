169ecc38d73d51359ab8c32aad962985b97606b0
aa6588 <aa6588@rit.edu>
2025-01-14 16:02:58 -0500

    added analysis

---
+++ src/Analysis/Rawanalysis.m
@@ -0,0 +1,6 @@
+%% Analysis of Raw data
+
+% organize VR vs Flat
+% average trials per lightness level for each participant
+% average lightness level for all participants (aggregate)
+% should end up with one RGB value per lightness per illum 
