a6bd489526e5a36165c351802e9c5c5bce6a1141
aa6588 <aa6588@rit.edu>
2025-02-10 16:01:21 -0500

    cleaned up plots

--- src/Analysis/VRplots.m
+++ src/Analysis/VRplots.m
@@ -65,7 +65,7 @@ figure;
 s = scatter(x,y,50,'black','filled','MarkerEdgeColor','k');
 alpha(s,0.2);
 hold on
-%scatter(uv_aims.r.(lightness)(:,1),uv_aims.r.(lightness)(:,2),50,[.8 .8 .8],'o')
+%scatter(uv_aims.w.(lightness)(:,1),uv_aims.w.(lightness)(:,2),50,[.8 .8 .8],'o')
 scatter(illum_uvY(1,1),illum_uvY(1,2),60,'ks')
 hold off
 xlabel('u')
@@ -93,6 +93,21 @@ xlabel('u')
 ylabel('v')
 title('Red Illuminant Achromatic Chromaticity Selections')
 end
+
+% CI plots
+avgRedData = avgDataCI(avgDataCI.Illuminant == 'r', :);
+avg_CIs = groupsummary(avgRedData,{'ParticipantID','Lightness'},'mean','CI');
+%reshape table
+% Reshape from long to wide format
+T_wide = unstack(avg_CIs, 'mean_CI', 'Lightness');
+% Convert the table to a matrix for plotting
+data_matrix = T_wide{:, {'L40', 'L55', 'L70'}};
+figure;
+h = bar(data_matrix, 'grouped');
+xlabel('Participant')
+ylabel('Constancy Index')
+title('[VR] Average Constancy Index per Lightness Under Red Illuminant')
+
 %GRID LIGHTNESS CHROMATICITY PLOTS (HEATMAP)
 % lightnessValues = {'L40', 'L55','L70'};
 % for i = 1:length(lightnessValues)
@@ -129,60 +144,32 @@ end
 % title(['Participant responses Red Illuminant ', lightness]);
 % grid on;
 % end
-
-% CI plots
-avgRedData = avgDataCI(avgDataCI.Illuminant == 'r', :);
-avg_CIs = groupsummary(avgRedData,{'ParticipantID','Lightness'},'mean','CI');
-%reshape table
-% Reshape from long to wide format
-T_wide = unstack(avg_CIs, 'mean_CI', 'Lightness');
-% Convert the table to a matrix for plotting
-data_matrix = T_wide{:, {'L40', 'L55', 'L70'}};
-figure;
-h = bar(data_matrix, 'grouped');
-xlabel('Participant')
-ylabel('Constancy Index')
-title('Average Constancy Index per Lightness Under Red Illuminant')
 %% GREEN
-% Count occurrences of each (x, y) pair
 lightnessValues = {'L40', 'L55','L70'};
 for i = 1:length(lightnessValues)
         lightness = lightnessValues{i};
         % Extract the subset for the current lightness
         currentData = greenData(greenData.Lightness == lightness, :);
-[unique_coords, ~, ind] = unique(currentData.uvY(:,1:2), 'rows');
-counts = accumarray(ind, 1);
-
-% Separate data for plotting
-x = unique_coords(:, 1);
-y = unique_coords(:, 2);
-c = counts;
-
-map = [0 .3 0; 0 0.5 0; 0 0.7 0];
-% Create scatter plot
+        x = currentData.uvY(:,1);
+        y = currentData.uvY(:,2);
+%x = redData.uvY(:,1);
+%y = redData.uvY(:,2);
 figure;
-scatter_handle = scatter(x, y, 50, c, 'filled');
-%num_steps = numel(unique(c));
-%colormap(copper(num_steps))
-colormap(map)
-cb = colorbar;
-cb.Ticks = min(c):max(c);
-%clim([min(c), max(c)]);
-
+s = scatter(x,y,50,'green','filled');
+alpha(s,0.2);
 hold on
 scatter(uv_aims.g.(lightness)(:,1),uv_aims.g.(lightness)(:,2),50,[.8 .8 .8],'o')
-scatter(illum_uvY(3,1),illum_uvY(3,2),60,'filled','gs')
-scatter(illum_uvY(1,1),illum_uvY(1,2),60,'k*')
-
-% Add labels and grid
-xlabel('u');
-ylabel('v');
-title(['Participant responses Green Illuminant ', lightness]);
-grid on;
+scatter(illum_uvY(2,1),illum_uvY(2,2),60,'filled','gs')
+scatter(illum_uvY(1,1),illum_uvY(1,2),60,'ks')
+hold off
+xlabel('u')
+ylabel('v')
+title('[VR] Green Illuminant Achromatic Chromaticity Selections')
 end
 
 % CI plots
-avg_CIs = groupsummary(greenData,{'ParticipantID','Lightness'},'mean','CI');
+avgGreenData = avgDataCI(avgDataCI.Illuminant == 'g', :);
+avg_CIs = groupsummary(avgGreenData,{'ParticipantID','Lightness'},'mean','CI');
 %reshape table
 % Reshape from long to wide format
 T_wide = unstack(avg_CIs, 'mean_CI', 'Lightness');
@@ -192,47 +179,32 @@ figure;
 h = bar(data_matrix, 'grouped');
 xlabel('Participant')
 ylabel('Constancy Index')
-title('Average Constancy Index per Lightness Under Green Illuminant')
+title('[VR] Average Constancy Index per Lightness Under Green Illuminant')
 %% blue
-% Count occurrences of each (x, y) pair
 lightnessValues = {'L40', 'L55','L70'};
 for i = 1:length(lightnessValues)
         lightness = lightnessValues{i};
         % Extract the subset for the current lightness
         currentData = blueData(blueData.Lightness == lightness, :);
-[unique_coords, ~, ind] = unique(currentData.uvY(:,1:2), 'rows');
-counts = accumarray(ind, 1);
+        x = currentData.uvY(:,1);
+        y = currentData.uvY(:,2);
 
-% Separate data for plotting
-x = unique_coords(:, 1);
-y = unique_coords(:, 2);
-c = counts;
-
-map = [0 0 0.3; 0 0 0.5; 0 0 0.7];
-% Create scatter plot
 figure;
-scatter_handle = scatter(x, y, 50, c, 'filled');
-%num_steps = numel(unique(c));
-%colormap(copper(num_steps))
-colormap(map)
-cb = colorbar;
-cb.Ticks = min(c):max(c);
-%clim([min(c), max(c)]);
-
+s = scatter(x,y,50,'blue','filled');
+alpha(s,0.2);
 hold on
 scatter(uv_aims.b.(lightness)(:,1),uv_aims.b.(lightness)(:,2),50,[.8 .8 .8],'o')
-scatter(illum_uvY(4,1),illum_uvY(4,2),60,'filled','bs')
-scatter(illum_uvY(1,1),illum_uvY(1,2),60,'k*')
-
-% Add labels and grid
-xlabel('u');
-ylabel('v');
-title(['Participant responses Blue Illuminant ', lightness]);
-grid on;
+scatter(illum_uvY(2,1),illum_uvY(2,2),60,'filled','bs')
+scatter(illum_uvY(1,1),illum_uvY(1,2),60,'ks')
+hold off
+xlabel('u')
+ylabel('v')
+title('[VR] Blue Illuminant Achromatic Chromaticity Selections')
 end
 
 % CI plots
-avg_CIs = groupsummary(blueData,{'ParticipantID','Lightness'},'mean','CI');
+avgBlueData = avgDataCI(avgDataCI.Illuminant == 'b', :);
+avg_CIs = groupsummary(avgBlueData,{'ParticipantID','Lightness'},'mean','CI');
 %reshape table
 % Reshape from long to wide format
 T_wide = unstack(avg_CIs, 'mean_CI', 'Lightness');
@@ -242,47 +214,32 @@ figure;
 h = bar(data_matrix, 'grouped');
 xlabel('Participant')
 ylabel('Constancy Index')
-title('Average Constancy Index per Lightness Under Blue Illuminant')
+title('[VR] Average Constancy Index per Lightness Under Blue Illuminant')
 %% yellow
-% Count occurrences of each (x, y) pair
 lightnessValues = {'L40', 'L55','L70'};
 for i = 1:length(lightnessValues)
         lightness = lightnessValues{i};
         % Extract the subset for the current lightness
         currentData = yellowData(yellowData.Lightness == lightness, :);
-[unique_coords, ~, ind] = unique(currentData.uvY(:,1:2), 'rows');
-counts = accumarray(ind, 1);
+        x = currentData.uvY(:,1);
+        y = currentData.uvY(:,2);
 
-% Separate data for plotting
-x = unique_coords(:, 1);
-y = unique_coords(:, 2);
-c = counts;
-
-map = [.4 .4 0; .6 .6 0; .8 .8 0];
-% Create scatter plot
 figure;
-scatter_handle = scatter(x, y, 50, c, 'filled');
-%num_steps = numel(unique(c));
-%colormap(copper(num_steps))
-colormap(map)
-cb = colorbar;
-cb.Ticks = min(c):max(c);
-%clim([min(c), max(c)]);
-
+s = scatter(x,y,50,'yellow','filled');
+alpha(s,0.2);
 hold on
 scatter(uv_aims.y.(lightness)(:,1),uv_aims.y.(lightness)(:,2),50,[.8 .8 .8],'o')
-scatter(illum_uvY(5,1),illum_uvY(5,2),60,'filled','ys')
-scatter(illum_uvY(1,1),illum_uvY(1,2),60,'k*')
-
-% Add labels and grid
-xlabel('u');
-ylabel('v');
-title(['Participant responses Yellow Illuminant ', lightness]);
-grid on;
+scatter(illum_uvY(2,1),illum_uvY(2,2),60,'filled','ys')
+scatter(illum_uvY(1,1),illum_uvY(1,2),60,'ks')
+hold off
+xlabel('u')
+ylabel('v')
+title('[VR] Yellow Illuminant Achromatic Chromaticity Selections')
 end
 
 % CI plots
-avg_CIs = groupsummary(yellowData,{'ParticipantID','Lightness'},'mean','CI');
+avgYellowData = avgDataCI(avgDataCI.Illuminant == 'y', :);
+avg_CIs = groupsummary(avgYellowData,{'ParticipantID','Lightness'},'mean','CI');
 %reshape table
 % Reshape from long to wide format
 T_wide = unstack(avg_CIs, 'mean_CI', 'Lightness');
@@ -292,4 +249,4 @@ figure;
 h = bar(data_matrix, 'grouped');
 xlabel('Participant')
 ylabel('Constancy Index')
-title('Average Constancy Index per Lightness Under Yellow Illuminant')
+title('[VR] Average Constancy Index per Lightness Under Yellow Illuminant')
